3bda897446f7c71d966081f608f179de09015ae6
Bigpurple52 <finalfantasy_cristalchronicle@hotmail.com>
2016-07-30 16:29:50 +0200

    Grosse refonte structurelle pour moins galérer
    J'avais un peu craqué :)

--- index.php
+++ index.php
@@ -1,75 +1,131 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>L'eau de Noé</title>
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/balneo.css">
-    </head>
- 
-    <body>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>L'eau de Noé</title>
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/balneo.css">
+</head>
+
+<body>
     <script src="js/jquery-3.1.0.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
-    <header id="headerIndex">
-        <div class="page-header">
-            <h2>" L'Eau de Noé "</h2>
-            <h2>Cours de natation / Aquagym / Aquabike / Aquaphobie</h2>
-            <h2>A votre domicile ou dans le centre NEOS à Montpellier (34)</h2>
-        </div>
-    </header>
-    
-    <nav id="sideMenu">        
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#">
-                        <h1 class="sideMenuTitle">Au centre Balnéo'H-Kylé</h1>
-                    </a>
-                </li>
-                <hr/>
-                <li>
-                    <a href="#">AquaDouce</a>
-                </li>
-                <li>
-                    <a href="#">AquaDynamic</a>
-                </li>
-                <li>
-                    <a href="#">AquaBike</a>
-                </li>
-                <li>
-                    <a href="#">AquaPhobie</a>
-                </li>
-                <li>
-                    <a href="#">Mix Aqua-training</a>
-                </li>
-                <li>
-                    <a href="#">Jardin Aquatique enfant</a>
-                </li>
-                <li>
-                    <a href="#">Stage de natation enfant</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <div class="wrapper">
+        <div class="box">
+            <div class="row row-offcanvas row-offcanvas-left">
 
-    <div id="AccueilContent">
-        
-    </div><!--Content-->
-    
-    <nav class="navbar navbar-fixed-bottom">
-        <div class="footer navbar-inverse">
-            <div class="footerContent">
-                <a href="#">Mix Aqua-training</a>
-                <span class="separator"></span>
-                <a href="#">Mix Aqua-training</a>
-                <span class="separator"></span>
-                <a href="#">Mix Aqua-training</a>
+
+                <!-- sidebar -->
+                <div class="column col-sm-3 col-xs-1 sidebar-offcanvas" id="sidebar">
+                    <ul class="sidebar-nav">
+                        <li class="sidebar-brand">
+                            <a href="#">
+                                <h2 class="sideMenuTitle">Au centre Balnéo'H-Kylé</h2>
+                            </a>
+                        </li>
+                        <hr/>
+                        <li>
+                            <a href="#">AquaDouce</a>
+                        </li>
+                        <li>
+                            <a href="#">AquaDynamic</a>
+                        </li>
+                        <li>
+                            <a href="#">AquaBike</a>
+                        </li>
+                        <li>
+                            <a href="#">AquaPhobie</a>
+                        </li>
+                        <li>
+                            <a href="#">Mix Aqua-training</a>
+                        </li>
+                        <li>
+                            <a href="#">Jardin Aquatique enfant</a>
+                        </li>
+                        <li>
+                            <a href="#">Stage de natation enfant</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /sidebar -->
+
+                <!-- main right col -->
+                <div class="column col-sm-9 col-xs-11" id="main">
+
+                    <!-- top nav -->
+                    <div class="navbar navbar-blue navbar-static-top">
+                        <nav class="collapse navbar-collapse no-margin" role="navigation">
+                            <h3>" L'Eau de Noé "</h3>
+                            <h3>Cours de natation / Aquagym / Aquabike / Aquaphobie</h3>
+                            <h3>A votre domicile ou dans le centre NEOS à Montpellier (34)</h3>
+                        </nav>
+                    </div>
+                    <!-- /top nav -->
+
+                    <div class="padding">
+                        <div class="full col-sm-9">
+
+                            <!-- content -->
+                            <div class="row">
+                                <div class="column acceuil-content">
+                                    <h1 class="center">"L'Eau de Noé", leçons aquatiques à Montpellier !</h1>
+                                    <ul>
+                                        <li>Jardin aquatique enfant de 3 à 5 ans: familiarisation avec l'eau dans un contexte ludique</li>
+                                        <li>Apprentissage de la natation  cours particulier de natation enfant à partir de 6 ans</li>
+                                        <li>Aquaphobie cours pour adulte permettant de vaincre la peur de l'eau</li>
+                                        <li>Aquagym Tonique cours d'aquagym dynamique</li>
+                                        <li>Mix Aqua-training cours comprenant différents ateliers: Aquabike, Step, Aquajump, ainsi que des exercices d'Aquagym avec matériel</li>
+                                    </ul>
+                                    <div>
+                                        Les séances de qualités sont toutes dispensés en petits groupes, dans la détente et bonne humeur!
+                                        L'eau de la piscine comprise entre 30 et 32° permettra des séances aquatiques agréable, ou  le travail est de rigueur!
+                                    </div>
+                                    <div>
+                                        Attention: les cours apprentissage de la natation sont  proposés uniquement pour les enfants, les dimensions du bassin (3m sur 6m) n'étant pas adapté à des entrainements adultes.
+                                    </div>
+                                    <div>
+                                        *  
+                                    </div>
+                                    <div>                                        
+                                        Les cours de natation et d'aquagym à Montpellier (ou dans les environs) sont dispensés à votre domicile
+                                        Des séances sont également proposé au centre de kinésithérapie NEOS SANTE à Montpellier
+                                    </div>
+                                    <div>
+                                        ACCES : 1 avenue Emile Bertin Sans, 34090 Montpellier
+                                        TRAM 1 Arrêt "Saint Eloi" 
+                                    </div> 
+                                    <div>
+                                        « Le sport va chercher la peur pour la dominer, la fatigue pour en triompher, la difficulté pour la vaincre »
+                                        <br/>Pierre de Coubertin
+                                    </div>
+                                </div>
+                            </div>
+                            <!--/row-->
+
+                            <div class="row" id="footer">
+                                <div class="col-sm-6 footerContent">
+                                    <a href="#">Mix Aqua-training</a>
+                                    <span class="separator"></span>
+                                    <a href="#">Mix Aqua-training</a>
+                                    <span class="separator"></span>
+                                    <a href="#">Mix Aqua-training</a>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- /col-9 -->
+                    </div>
+                    <!-- /padding -->
+                </div>
+                <!-- /main -->
+
             </div>
         </div>
-    </nav>
-    
-    </body>
+    </div>
+</body>
+
 </html>
