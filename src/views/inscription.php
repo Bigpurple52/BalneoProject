69bf8a3e05e76bdcebf420da05073c0da8298f7b
SkunkyDu52 <dessez.maxime@laposte.net>
2016-08-02 21:21:16 +0200

    commit
    commit

--- src/views/inscription.php
+++ src/views/inscription.php
@@ -1,63 +1,129 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr-FR">
+
     <head>
         <meta charset="utf-8" />
-        <title>Inscription</title>
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1">
+        <meta name="description" content="activité aquatique à Montpellier : Aquadynamic">
+        <meta name="keywords" content="Activités aquatiques;Aquadynamic;">
+        <title>aqua-balneo.fr : Aquadynamic</title>
         <link rel="stylesheet" href="../../css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="../../css/bootstrap.min.css">
         <link rel="stylesheet" href="../../css/balneo.css">
     </head>
- 
+
     <body>
-    <script src="../../js/jquery-3.1.0.min.js"></script>
-    <script src="../../js/bootstrap.min.js"></script>
-    
-    <header id="headerIndex">
-       
-    </header>
-    
-    
-    <nav id="sideMenu">        
-       
-    </nav>
-    
-    <div id="inscriptionContent">
-        <div class="row">
-            <div class="col-lg-6 col-lg-offset-3">
-                <h1>Inscription</h1><hr class="hrtrait"/>
-                <h2>Vos informations personnelles</h2><hr />
-                <form role="form" name="inscription1" method="POST" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>>
-                    <div class="form-group" id="login-form">             
-                        <div class="form-group">
-                            <label class="control-label" for="nom">Nom</label>
-                            <input id="nom" class="form-control required nom" type="text" placeholder="Mon nom" name="nom" autofocus required value="<?php if(isset($_POST['nom'])) { echo htmlentities($_POST['nom']);}?>" data-placement="right" data-trigger="manual" data-content="Doit contenir entre 2 et 20 caracteres, uniquement de lettres"/>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="prenom">Prenom</label>
-                            <input id="prenom" class="form-control required prenom" type="text" placeholder="Mon prenom" name="prenom" required value="<?php if(isset($_POST['prenom'])) { echo htmlentities($_POST['prenom']);}?>" data-placement="right" data-trigger="manual" data-content="Doit contenir entre 2 et 20 caracteres, uniquement de lettres"/>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="pass">Mot de passe</label>
-                            <input id="pass" class="form-control required pass" type="password" placeholder="Mon mot de passe" name="pass" data-placement="right" data-trigger="manual" data-content="Doit contenir au moins 6 caracteres, dont au moins un chiffre, une lettre majuscule et minuscule."/>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="pass2">Confirmation du mot de passe</label>
-                            <input id="pass2" class="form-control required pass2" type="password" placeholder="Mon mot de passe" name="pass2" data-placement="right" data-trigger="manual" data-content="Doit etre identique au precedent mot de passe"/>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="mail">Email</label>
-                            <input id="mail" class="form-control required email" type="text" placeholder="Mon email" name="mail" data-placement="right" data-trigger="manual" value="<?php if(isset($_POST['mail'])) { echo htmlentities($_POST['mail']);}?>" data-content="Doit etre une adresse mail valide (user@gmail.com)"/>
-                        </div>
-                        <hr/>
-                        <input type="submit" class="btn btn-primary" name="submit" value="S'incrire"/>
-                    </div>
-                </form>
+        <div class="container-fluid">
+            <div class="row">
+                <!-- top nav -->
+                <header>
+                    <nav class="collapse navbar-collapse">
+                        <br/>
+                        <h3>Au centre Balnéo K-Hylé</h3>
+                        <br/>
+                    </nav>
+                </header>
+                <!-- /top nav -->
             </div>
-        </div> 
-    </div><!--Content-->
-    
-    <footer id="footer">
-    </footer>
-    
+            <div class="wrapper">
+                <div class="row">
+                    <!-- sidebar -->
+                    <nav class="col-sm-2 white" role="navigation">
+                        <ul class="sidebar-nav">
+                            <li class="sidebar-brand">
+                                <a href="../../index.html" rel="contents">
+                                    <h2 class="sideMenuTitle">Accueil</h2>
+                                    <hr/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./aquadouce.html" rel="section">AquaDouce</a>
+                            </li>
+                            <li>
+                                <a href="#" rel="section">AquaDynamic</a>
+                            </li>
+                            <li>
+                                <a href="./aquabike.html" rel="section">AquaBike</a>
+                            </li>
+                            <li>
+                                <a href="./aquaphobie.html" rel="section">AquaPhobie</a>
+                            </li>
+                            <li>
+                                <a href="./aquatraining.html" rel="section">Mix Aqua-training</a>
+                            </li>
+                            <li>
+                                <a href="./jardin.html" rel="section">Jardin Aquatique enfant</a>
+                            </li>
+                            <li>
+                                <a href="./stage.html" rel="section">Stage de natation enfant</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!-- /sidebar -->
+
+                    <!-- content -->
+                    <div class="col-sm-10">
+                        <h2 class="inscription">Inscription</h2>
+                        <hr class="hrtrait"/>
+                        <h2 class="inscription">Vos informations personnelles</h2><hr />
+                        <form role="form" class="well" name="inscription1" method="POST" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>>
+                            <div class="form-group" id="login-form">
+                                <div class="form-group">
+                                    <label class="control-label" for="nom">Nom</label>
+                                    <input id="nom" class="form-control" type="text" placeholder="Mon nom" name="nom" autofocus  value="" data-placement="right" data-trigger="manual" data-content="Doit contenir entre 2 et 20 caracteres, uniquement de lettres"/>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label" for="prenom">Prénom</label>
+                                    <input id="prenom" class="form-control" type="text" placeholder="Mon prenom" name="prenom"  value="" data-placement="right" data-trigger="manual" data-content="Doit contenir entre 2 et 20 caracteres, uniquement de lettres"/>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label" for="pass">Mot de passe</label>
+                                    <input id="pass" class="form-control" type="password" placeholder="Mon mot de passe" name="pass" data-placement="right" data-trigger="manual" data-content="Doit contenir au moins 6 caracteres, dont au moins un chiffre, une lettre majuscule et minuscule."/>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label" for="pass2">Confirmation du mot de passe</label>
+                                    <input id="pass2" class="form-control" type="password" placeholder="Mon mot de passe" name="pass2" data-placement="right" data-trigger="manual" data-content="Doit etre identique au precedent mot de passe"/>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label" for="mail">Email</label>
+                                    <input id="mail" class="form-control" type="text" placeholder="Mon email" name="mail" data-placement="right" data-trigger="manual" value="" data-content="Doit etre une adresse mail valide (user@gmail.com)"/>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label" for="tel">Telephone</label>
+                                    <input id="tel" class="form-control" type="text" placeholder="Mon numéro de téléphone" name="tel" data-placement="right" data-trigger="manual" value="" data-content="Doit être un numéro de téléphone valide(0611223344)"/>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label" for="datenaissance">Date de naissance</label>
+                                    <input id="datenaissance" class="form-control" type="datetime" placeholder="Date de naissance" name="Date de naissance" data-placement="right" data-trigger="manual" value="" data-content="Date de naissance"/>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label" for="lieunaissance">Lieu de naissance</label>
+                                    <input id="lieunaissance" class="form-control" type="text" placeholder="Mon lieu de naissance" name="tel" data-placement="right" data-trigger="manual" value="" data-content="Doit être un numéro de téléphone valide(0611223344)"/>
+                                </div>
+                                <hr/>
+                                <input type="submit" class="btn btn-primary" name="submit" value="S'incrire"/>
+                            </div>
+                        </form>
+                    </div>
+
+                    <footer>
+                        <div class="col-sm-6 footerContent">
+                            <a href="#" rel="section">Mix Aqua-training</a>
+                            <span class="separator"></span>
+                            <a href="#" rel="section">Mix Aqua-training</a>
+                            <span class="separator"></span>
+                            <a href="#" rel="section">Mix Aqua-training</a>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+            <script src="../../js/jquery-3.1.0.min.js"></script>
+            <script src="../../js/bootstrap.min.js"></script>
+
+        </div><!--Content-->
+
+        <footer id="footer">
+        </footer>
+
     </body>
 </html>
