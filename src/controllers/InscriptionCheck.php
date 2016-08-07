01607d029e069c9b0645855f1ca719910bc56a9b
SkunkyDu52 <dessez.maxime@laposte.net>
2016-08-07 12:04:04 +0200

    Mail automatique confirmation inscription
    Mail automatique confirmation inscription
    Need serveur smtp pour tester donc need passer ne prod mais ca semble
    correct!

--- src/controllers/InscriptionCheck.php
+++ src/controllers/InscriptionCheck.php
@@ -50,6 +50,61 @@ function insertUser()
     try {
         if ($stmt->execute()) {
             $etatInscription = true;
+            if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) {
+                $passage_ligne = "\r\n";
+            } else {
+                $passage_ligne = "\n";
+            }
+            //=====Déclaration des messages au format texte et au format HTML.
+            $message_txt = "Bonjour " . $prenom . $nom . ", "
+                    . $passage_ligne . "Vous venez de vous inscrire sur le site aqua-balneo.fr."
+                    . $passage_ligne . "Ce mail est un récapitulatif de vos informations."
+                    . $passage_ligne . $passage_ligne
+                    . $passage_ligne . "Mon identifiant de connexion : " . $email
+                    . $passage_ligne . "Mon mot de passe : " . $filteredPost['password']
+                    . $passage_ligne . ">A bientot dans notre centre K-Hylé!";
+            $message_html = "<html><head></head><body>"
+                    . "<p>Bonjour " . $prenom . $nom . ",</p>"
+                    . "<p>vous venez de vous inscrire sur le site <b>aqua-baleno.fr</b>.</p>"
+                    . "<p>Ce mail est un récapitulatif de vos informations.</p>"
+                    . "<br>"
+                    . "<p>Mon identifiant de connexion : " . $email . "</p>"
+                    . "<p>Mon mot de passe : " . $filteredPost['password'] . "</p>"
+                    . "<br>"
+                    . "<p>A bientot dans notre centre K-Hylé!"
+                    . "</body></html>";
+            //==========
+            //=====Création de la boundary
+            $boundary = " -----=" . md5(rand());
+            //==========
+            //=====Définition du sujet.
+            $sujet = "Inscription au site www.aqua-balneo.fr";
+            //=========
+            //=====Création du header de l'e-mail.
+            $header = "From: \"WeaponsB\"<maxime.2ssez@laposte.net>" . $passage_ligne;
+            $header.= "Reply-to: \"WeaponsB\" <maxime.2ssez@laposte.net>" . $passage_ligne;
+            $header.= "MIME-Version: 1.0" . $passage_ligne;
+            $header.= "Content-Type: multipart/alternative;" . $passage_ligne . " boundary=\"$boundary\"" . $passage_ligne;
+            //==========
+            //=====Création du message.
+            $message = $passage_ligne . "--" . $boundary . $passage_ligne;
+            //=====Ajout du message au format texte.
+            $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"" . $passage_ligne;
+            $message.= "Content-Transfer-Encoding: 8bit" . $passage_ligne;
+            $message.= $passage_ligne . $message_txt . $passage_ligne;
+            //==========
+            $message.= $passage_ligne . "--" . $boundary . $passage_ligne;
+            //=====Ajout du message au format HTML
+            $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"" . $passage_ligne;
+            $message.= "Content-Transfer-Encoding: 8bit" . $passage_ligne;
+            $message.= $passage_ligne . $message_html . $passage_ligne;
+            //==========
+            $message.= $passage_ligne . "--" . $boundary . "--" . $passage_ligne;
+            $message.= $passage_ligne . "--" . $boundary . "--" . $passage_ligne;
+            //==========
+            //=====Envoi de l'e-mail.
+            mail($email, $sujet, $message, $header);
+            //==========
         } else {
             $etatInscription = false;
         }
