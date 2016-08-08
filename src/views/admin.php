8a1818893a486beef8dd6ec78ac2c55d2fd7e0ae
Bigpurple52 <finalfantasy_cristalchronicle@hotmail.com>
2016-08-08 22:49:05 +0200

    nique sa mere

--- src/views/admin.php
+++ src/views/admin.php
@@ -13,8 +13,7 @@ if (!isset($_SESSION['user']) && $_SESSION['user'] !== 'k-hyle@aqua.balneo.fr') 
     }
 } elseif (isset($_SESSION['user']) && $_SESSION['user'] === 'k-hyle@aqua-balneo.fr') {
     try {
-        $sql = new PDO('mysql:host=aquabalncxaquadb.mysql.db;dbname=balneodb', 'aquabalncxaquadb', '3Kp6aSDbgkK7');
-        $coOk = true;
+        $sql = new PDO('mysql:host=aquabalncxaquadb.mysql.db;dbname=aquabalncxaquadb', 'aquabalncxaquadb', '3Kp6aSDbgkK7');
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage() . "<br/>";
         die();
