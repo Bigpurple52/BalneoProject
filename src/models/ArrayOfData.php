c3f058779031ab0ab6b9a29581b82c6096658017
SkunkyDu52 <dessez.maxime@laposte.net>
2016-08-21 17:16:55 +0200

    th
    srth

--- src/models/ArrayOfData.php
+++ src/models/ArrayOfData.php
@@ -1,64 +1,239 @@
 <?php
 
 $tableau = [
+    //AquaDouce
     [
-        'title' => 'Aquadouce',
+        'title' => 'AquaDouce',
         'start' => '2016-08-22T10:30:00',
         'end' => '2016-08-22T11:15:00',
         'available' => 1
     ],
     [
-        'title' => 'Aquadouce',
+        'title' => 'AquaDouce',
         'start' => '2016-08-22T16:30:00',
         'end' => '2016-08-22T17:15:00',
         'available' => 1
     ],
     [
-        'title' => 'Aquadouce',
+        'title' => 'AquaDouce',
         'start' => '2016-08-23T11:00:00',
         'end' => '2016-08-23T11:45:00',
         'available' => 0
     ],
     [
-        'title' => 'Aquadouce',
+        'title' => 'AquaDouce',
         'start' => '2016-08-23T16:30:00',
         'end' => '2016-08-23T17:15:00',
         'available' => 1
     ],
     [
-        'title' => 'Aquadouce',
+        'title' => 'AquaDouce',
         'start' => '2016-08-24T11:00:00',
         'end' => '2016-08-24T11:45:00',
         'available' => 0
     ],
     [
-        'title' => 'Aquadouce',
+        'title' => 'AquaDouce',
         'start' => '2016-08-25T11:00:00',
         'end' => '2016-08-25T11:45:00',
         'available' => 0
     ],
     [
-        'title' => 'Aquadouce',
+        'title' => 'AquaDouce',
         'start' => '2016-08-25T16:30:00',
         'end' => '2016-08-25T17:15:00',
         'available' => 0
     ],
     [
-        'title' => 'Aquadouce',
+        'title' => 'AquaDouce',
         'start' => '2016-08-26T10:30:00',
         'end' => '2016-08-26T11:15:00',
         'available' => 1
     ],
     [
-        'title' => 'Aquadouce',
+        'title' => 'AquaDouce',
         'start' => '2016-08-26T16:30:00',
         'end' => '2016-08-26T17:15:00',
         'available' => 0
     ],
     [
-        'title'=>'Aquadouce',
-        'start'=>'2016-08-27T09:15:00',
-        'end'=> '2016-08-27T10:00:00',
-        'available'=> 0
-    ]
+        'title' => 'AquaDouce',
+        'start' => '2016-08-27T09:15:00',
+        'end' => '2016-08-27T10:00:00',
+        'available' => 0
+    ],
+    //AquaDynamic
+    [
+        'title' => 'AquaDynamic',
+        'start' => '2016-08-22T18:30:00',
+        'end' => '2016-08-22T19:15:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'AquaDynamic',
+        'start' => '2016-08-23T12:00:00',
+        'end' => '2016-08-23T12:45:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'AquaDynamic',
+        'start' => '2016-08-24T12:00:00',
+        'end' => '2016-08-24T12:45:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'AquaDynamic',
+        'start' => '2016-08-25T17:30:00',
+        'end' => '2016-08-25T18:15:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'AquaDynamic',
+        'start' => '2016-08-26T11:30:00',
+        'end' => '2016-08-26T12:15:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'AquaDynamic',
+        'start' => '2016-08-26T17:30:00',
+        'end' => '2016-08-26T18:15:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'AquaDynamic',
+        'start' => '2016-08-27T12:15:00',
+        'end' => '2016-08-27T13:00:00',
+        'available' => 0
+    ],
+    //AquaBike
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-22T12:30:00',
+        'end' => '2016-08-22T13:15:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-22T17:30:00',
+        'end' => '2016-08-22T18:15:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-23T13:00:00',
+        'end' => '2016-08-23T13:45:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-23T17:30:00',
+        'end' => '2016-08-23T18:15:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-24T13:00:00',
+        'end' => '2016-08-24T13:45:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-25T13:00:00',
+        'end' => '2016-08-25T13:45:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-25T18:30:00',
+        'end' => '2016-08-25T19:15:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-26T12:30:00',
+        'end' => '2016-08-26T13:15:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-26T18:30:00',
+        'end' => '2016-08-26T19:15:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'AquaBike',
+        'start' => '2016-08-27T10:15:00',
+        'end' => '2016-08-27T11:00:00',
+        'available' => 0
+    ],
+    //MixAquaTraining
+    [
+        'title' => 'MixAquaTraining',
+        'start' => '2016-08-22T11:30:00',
+        'end' => '2016-08-22T12:15:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'MixAquaTraining',
+        'start' => '2016-08-23T18:30:00',
+        'end' => '2016-08-23T19:15:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'MixAquaTraining',
+        'start' => '2016-08-24T17:00:00',
+        'end' => '2016-08-24T17:45:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'MixAquaTraining',
+        'start' => '2016-08-25T12:00:00',
+        'end' => '2016-08-25T12:45:00',
+        'available' => 0
+    ],
+    [
+        'title' => 'MixAquaTraining',
+        'start' => '2016-08-27T11:15:00',
+        'end' => '2016-08-27T12:00:00',
+        'available' => 0
+    ],
+    //AquaPhobie
+    [
+        'title' => 'AquaPhobie',
+        'start' => '2016-08-27T17:00:00',
+        'end' => '2016-08-27T17:45:00',
+        'available' => 1
+    ],
+    //JardinAquatique
+    [
+        'title' => 'JardinAquatique',
+        'start' => '2016-08-24T15:00:00',
+        'end' => '2016-08-24T15:40:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'JardinAquatique',
+        'start' => '2016-08-27T14:00:00',
+        'end' => '2016-08-27T14:40:00',
+        'available' => 1
+    ],
+    //AquaBaby
+    [
+        'title' => 'AquaBaby',
+        'start' => '2016-08-24T16:00:00',
+        'end' => '2016-08-24T16:40:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'AquaBaby',
+        'start' => '2016-08-27T15:00:00',
+        'end' => '2016-08-27T15:40:00',
+        'available' => 1
+    ],
+    [
+        'title' => 'AquaBaby',
+        'start' => '2016-08-27T16:00:00',
+        'end' => '2016-08-27T16:40:00',
+        'available' => 1
+    ],
 ];
