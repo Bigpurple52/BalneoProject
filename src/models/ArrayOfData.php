065aa76c0e716498478c85f5990aefd342af88ec
SkunkyDu52 <dessez.maxime@laposte.net>
2016-08-21 15:04:57 +0200

    xwfg
    dsqrg

---
+++ src/models/ArrayOfData.php
@@ -0,0 +1,70 @@
+<?php
+
+$tableau = [
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-22T10:30:00',
+        'end' => '2016-08-22T11:15:00',
+        'available' => true
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-22T16:30:00',
+        'end' => '2016-08-22T17:15:00',
+        'available' => true
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-23T11:00:00',
+        'end' => '2016-08-23T11:45:00',
+        'available' => false
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-23T16:30:00',
+        'end' => '2016-08-23T17:15:00',
+        'available' => true
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-24T11:00:00',
+        'end' => '2016-08-24T11:45:00',
+        'available' => false
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-25T11:00:00',
+        'end' => '2016-08-25T11:45:00',
+        'available' => false
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-25T16:30:00',
+        'end' => '2016-08-25T17:15:00',
+        'available' => false
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-26T10:30:00',
+        'end' => '2016-08-26T11:15:00',
+        'available' => true
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-26T10:30:00',
+        'end' => '2016-08-26T11:15:00',
+        'available' => true
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-26T16:30:00',
+        'end' => '2016-08-26T17:15:00',
+        'available' => false
+    ],
+    [
+        'title' => 'Aquadouce',
+        'start' => '2016-08-26T16:30:00',
+        'end' => '2016-08-27T10:00:00',
+        'available' => false
+    ]
+];
